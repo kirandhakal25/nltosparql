00fb023ae01b53ddd2d42c4028669d2c77adebe9
Kiran Dhakal <st121108@ait.asia>
2020-07-17 16:12:36 +0700

    Modified the lexicon slightly to accomodate the same entity being a subject and an object

--- ontotriple.py
+++ ontotriple.py
@@ -2,6 +2,7 @@ from lexicon import Lexicon, LexicalEntry
 import stanza
 import nltk
 from nltk.corpus import wordnet as wn
+import pprint
 
 """
 Convert user triple to ontology triples.
@@ -35,16 +36,51 @@ Reference case 2:
 """
 
 # nltk.download('wordnet')
+def map_user_to_lexicon(lexicon, user_triple):
+    print("=====from map_user_to_lexicon=====")
+    x_lexicon = user_to_lexicon_subject(lexicon, user_triple[0])
+    print("x_lexicon", x_lexicon['label'], x_lexicon['associated_predicate'], x_lexicon['associated_object'])
+    z_lexicon = user_to_lexicon_object(lexicon, user_triple[2])
+    print("z_lexicon", z_lexicon['label'], z_lexicon['associated_predicate'], z_lexicon['associated_subject'])
+    if (x_lexicon['associated_object'] == z_lexicon['label'] or x_lexicon['associated_object'] == lexicon.entries[z_lexicon['label']].type) and \
+            x_lexicon['associated_predicate'] == z_lexicon['associated_predicate']:
+        print("Oh Yeah")
+    else:
+        print("Nay")
 
-
-def map_user2lexicon(input_lexicon, subject):
+def user_to_lexicon_subject(input_lexicon, subject):
+    min_med = 1000
+    x_lexicon = ""
+    pprint.pprint(input_lexicon.entries['Course'].ontotriples)
     for entry in input_lexicon.entries:
-        med = nltk.edit_distance(subject, input_lexicon.entries[entry].label)
-        # print(med)
+        for ontotriple in input_lexicon.entries[entry].ontotriples:
+            if ontotriple['category'] == "subject":
+                med = nltk.edit_distance(subject.lower(), input_lexicon.entries[entry].label.lower())
+                if med < min_med:
+                    min_med = med
+                    x_lexicon = ontotriple
+                    # x_lexicon = input_lexicon.entries[entry].label
+    print(subject, x_lexicon)
+    # z = input_lexicon.entries[x_lexicon].associated_object
+    #
+    # return input_lexicon.entries[x_lexicon]
+    return x_lexicon
 
-        if med == 0:
-            print("Found exact match!")
-            return input_lexicon.entries[entry]
+def user_to_lexicon_object(input_lexicon, obj):
+    min_med = 1000
+    z_lexicon = ""
+    for entry in input_lexicon.entries:
+        for ontotriple in input_lexicon.entries[entry].ontotriples:
+            if ontotriple['category'] == "object":
+                med = nltk.edit_distance(obj.lower(), input_lexicon.entries[entry].label.lower())
+                if med < min_med:
+                    min_med = med
+                    z_lexicon = ontotriple
+                    # z_lexicon = input_lexicon.entries[entry].label
+    # return input_lexicon.entries[z_lexicon]
+    return z_lexicon
+
+
 
 
 # Constructing lexicon
@@ -54,54 +90,114 @@ instr_entry_args = {
     'label': 'Instructor',
     'part_of_speech': 'noun',
     'type': 'Class',
-    'ontotriple_category': 'subject',
-    'associated_predicate': 'teaches',
-    'associated_object': 'Course'
+    'ontotriples':
+    [
+        {
+            'label': 'Instructor',
+            'category': 'subject',
+            'associated_predicate': 'teaches',
+            'associated_object': 'Course'
+        },
+        {
+            'label': 'Instructor',
+            'category': 'object',
+            'associated_predicate': 'isTaughtBy',
+            'associated_subject': 'Course'
+        }
+    ]
 }
 
 teach_entry_args = {
     'label': 'teaches',
     'part_of_speech': 'verb',
     'type': 'predicate',
-    'ontotriple_category': 'predicate',
-    'associated_subject': 'Instructor',
-    'associated_object': 'Course'
+    'ontotriples':
+    [
+        {
+            'label': 'teaches',
+            'category': 'predicate',
+            'associated_subject': 'Instructor',
+            'associated_object': 'Course'
+        }
+    ]
 }
 
 is_taught_by_entry_args = {
     'label': 'isTaughtBy',
     'part_of_speech': 'verb',
     'type': 'predicate',
-    'ontotriple_category': 'predicate',
-    'associated_subject': 'course',
-    'associated_object': 'instructor'
+    'ontotriples':
+    [
+        {
+            'label': 'isTaughtBy',
+            'category': 'predicate',
+            'associated_subject': 'course',
+            'associated_object': 'instructor'
+        }
+    ]
 }
 
 course_entry_args = {
     'label': 'Course',
     'part_of_speech': 'noun',
     'type': 'class',
-    'ontotriple_category': 'object',
-    'associated_predicate': 'teaches',
-    'associated_subject': 'Instructor'
+    'ontotriples':
+    [
+        {
+            'label': 'Course',
+            'category': 'object',
+            'associated_predicate': 'teaches',
+            'associated_subject': 'Instructor'
+        },
+        {
+            'label': 'Course',
+            'category': 'subject',
+            'associated_predicate': 'isTaughtBy',
+            'associated_object': 'Instructor'
+        }
+    ]
 }
 
 nlu_entry_args = {
     'label': 'NLU',
     'part_of_speech': 'noun',
     'type': 'Course',
-    'ontotriple_category': 'object',
-    'associated_predicate': 'isTaughtBy',
-    'associated_subject': 'Dung'
+    'ontotriples':
+    [
+        {
+            'label': 'NLU',
+            'category': 'object',
+            'associated_predicate': 'teaches',
+            'associated_subject': 'ProfDung'
+        },
+        {
+            'label': 'NLU',
+            'category': 'subject',
+            'associated_predicate': 'isTaughtBy',
+            'associated_object': 'ProfDung'
+        }
+    ]
 }
 
 dung_entry_args = {
-    'label': 'Dung',
+    'label': 'ProfDung',
     'part_of_speech': 'noun',
     'type': 'instructor',
-    'ontotriple_category': 'subject',
-    'associated_predicate': 'teaches',
-    'associated_subject': 'NLU'
+    'ontotriples':
+    [
+        {
+            'label': 'ProfDung',
+            'category': 'subject',
+            'associated_predicate': 'teaches',
+            'associated_object': 'NLU'
+        },
+        {
+            'label': 'ProfDung',
+            'category': 'object',
+            'associated_predicate': 'isTaughtBy',
+            'associated_subject': 'NLU'
+        }
+    ]
 }
 
 lexicon.add_entry(LexicalEntry(**instr_entry_args))
@@ -110,22 +206,23 @@ lexicon.add_entry(LexicalEntry(**course_entry_args))
 lexicon.add_entry(LexicalEntry(**nlu_entry_args))
 lexicon.add_entry(LexicalEntry(**dung_entry_args))
 
-input_target = "instructor"
-input_user_triple = ("instructor", "teaches", "NLU")
+if __name__ == '__main__':
 
-instructor = wn.synset(f'{input_user_triple[0]}.n.01')
+    input_target = "instructor"
+    input_user_triple = ("instructor", "teaches", "NLU")
+    # instructor = wn.synset(f'{input_user_triple[0]}.n.01')
+    result = map_user_to_lexicon(lexicon, input_user_triple)
+    # result = user_to_lexicon_subject(lexicon, input_user_triple[0])
+    triple_subject = None
 
-result = map_user2lexicon(lexicon, input_user_triple[0])
-triple_subject = None
+    # if result.ontotriple_category == "subject":
+    #     triple_subject = result
 
-if result.ontotriple_category == "subject":
-    triple_subject = result
+    # triple_predicate = triple_subject.associated_predicate
+    # triple_object = triple_subject.associated_object
 
-triple_predicate = triple_subject.associated_predicate
-triple_object = triple_subject.associated_object
+    # query = f'select ?{input_target} where {{ {triple_subject.label} {triple_predicate} {triple_object} . }}'
 
-query = f'select ?{input_target} where {{ {triple_subject.label} {triple_predicate} {triple_object} . }}'
-
-print("=========RESULTS=============")
-print(query)
-print("End")
+    print("=========RESULTS=============")
+    # print(query)
+    print("End")
