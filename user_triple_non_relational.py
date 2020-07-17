c550876b62e61c724c06afb1676f74aaad0058ee
prabina <st121107@ait.ac.th>
2020-07-17 19:10:05 +0700

    triples of non relational

--- user_triple_non_relational.py
+++ user_triple_non_relational.py
@@ -7,17 +7,28 @@ def get_user_triples(doc):
     objs = []
 
     is_compound, rule = compound_non_reln(doc)
+    # for sentence in doc.sentences:
+    #     for word in sentence.words:
+    #         print(word.text, word.upos)
 
     if is_compound:
-        queries = break_compound_non_reln(doc, rule)
+        queries, cc = break_compound_non_reln(doc, rule)
+        # print(queries)
         for query in queries:
             ss, ps, os = generate_triple_non_reln(query)
+            # print(ss)
+            # print(ps)
             subjects = subjects + ss
             predicates = predicates + ps
             objs = objs + os
-        return (queries)
     else:
-        print("tadaaa")
+        ss, ps, os = generate_triple_non_reln(doc.text)
+        # print(ss)
+        # print(ps)
+        subjects = subjects + ss
+        predicates = predicates + ps
+        objs = objs + os
+    return zip(subjects, predicates, objs)
 
 def compound_non_reln(doc):
     upos_string = ""
@@ -26,10 +37,17 @@ def compound_non_reln(doc):
 
     compound_upos_patterns_rule_1 = [
         "AUX (DET)? NOUN CCONJ NOUN",
+        "AUX (DET)? NOUN CCONJ PROPN",
+        "AUX (DET)? PROPN CCONJ NOUN",
+        "AUX (DET)? PROPN CCONJ PROPN"
     ]
 
     compound_upos_patterns_rule_2 = [
         "ADJ CCONJ ADJ NOUN"
+    ]
+
+    compound_upos_patterns_rule_3 = [
+        "AUX (DET)? NOUN ADP PROPN CCONJ PROPN"
     ]
 
     for sentence in doc.sentences:
@@ -45,6 +63,11 @@ def compound_non_reln(doc):
         if comp:
             compound_flag = bool(comp)
             rule = 2
+    for pattern in compound_upos_patterns_rule_3:
+        comp = re.search(pattern, upos_string)
+        if comp:
+            compound_flag = bool(comp)
+            rule = 3
     return compound_flag, rule
 
 def break_compound_non_reln(doc, rule):
@@ -103,9 +126,8 @@ def break_compound_non_reln(doc, rule):
 
         queries.append(s1.strip())
         queries.append(s2.strip())
-        print(s1)
-        print(s2)
-        return queries
+        cc = wg
+        return queries, cc
 
     elif rule == 2:
         queries = []
@@ -154,11 +176,121 @@ def break_compound_non_reln(doc, rule):
 
         queries.append(s1.strip())
         queries.append(s2.strip())
-        print(s1)
-        print(s2)
-        return queries
+        cc = wi
+
+        return queries, cc
+
+    elif rule == 3:
+        queries = []
+        wi = ""
+        wh = ""
+        wj = ""
+        cc_flag = True
+        conj_flag = True
+        for sentence in doc.sentences:
+            for word in sentence.words:
+                # print(word.text, '-', sentence.words[word.head - 1].text, word.deprel)
+
+                if word.deprel == 'cc' and cc_flag:
+                    wi = word
+                    # print('------------')
+                    # print(wi)
+                    # print(wi.text)
+                    cc_flag = False
+                if word.deprel == 'conj' and conj_flag:
+                    wh = sentence.words[word.head - 1]
+                    wj = word
+                    # print(wh.text, wj.text)
+                    conj_flag = False
+
+        s1 = ""
+        s1_flag = True
+        s2 = ""
+        s2_flag = True
+        for sentence in doc.sentences:
+            for word in sentence.words:
+                # print(word.text, word.upos, word.deprel, '-', word.head, '-', sentence.words[word.head - 1].text)
+
+        #         # Conditions to change the flags
+                if word.text == wh.text:
+                    s2_flag = False
+
+                if word.text == wj.text:
+                    s2_flag = True
+
+                # Add words to Question s1
+                if s1_flag:
+                    s1 = s1 + " " + word.text
+
+                # Add words to Question s2
+                if s2_flag:
+                    s2 = s2 + " " + word.text
+
+                # Conditions to change the flags
+                if word.text == wh.text:
+                    s1_flag = False
+
+                if word.text == wj.text:
+                    s1_flag = True
+
+        queries.append(s1.strip())
+        queries.append(s2.strip())
+        cc = wi
+        # print(s1)
+        # print(s2)
+
+        return queries, cc
+
+
 
 def generate_triple_non_reln(query):
+    doc = nlp(query)
+    subjects = []
+    objects = []
+    predicates = []
+    pred = ""
+    subject_flag = True
+    prep_flag = True
+    wz= ''
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            if word.deprel == 'root':
+                root = word
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            if word.deprel == 'nsubj' and subject_flag:
+                if word.upos == 'NOUN':
+                    wx = word
+                elif word.upos == 'PRON':
+                    wx = root
+                # print(wx)
+                subject_flag = False
+            # print(word.text, word.upos, word.deprel, '-', word.head, '-', sentence.words[word.head - 1].text)
+            if word.deprel == 'case' and prep_flag:
+                if word.text == 'of':
+                    wz = sentence.words[word.head - 1].text
+                    wy = word.text
+                    prep_flag = False
+                    pred = wx.text + '_' + wy
+                    subjects.append(wz)
+                elif word.text == 'in':
+                    pred = wx.text
+                    wz = sentence.words[word.head - 1].text
+
+                    subjects.append(wz)
+                    # print('--------')
+                    # print(word.deprel, word.text, '-', word.head, '-', sentence.words[word.head - 1].text)
+
+
+        predicates.append(pred)
+        objects.append('?k')
+
+    # print(subjects)
+    # print(predicates)
+    # print(objects)
+
+    return subjects, predicates, objects
+
 
 if __name__ == '__main__':
     param_dict = {
@@ -169,13 +301,16 @@ if __name__ == '__main__':
     }
     nlp = stanza.Pipeline(**param_dict)
     # doc = nlp("What is the population and area of the most populated state?")
-    doc = nlp("Which is the shortest and longest river in America?")
+    # doc = nlp("Which is the shortest and longest river in America?")
+    # doc = nlp("Who are the professors of NLU and DSA?")
     # doc = nlp("Which is the highest mountain in Germany?")
     # doc = nlp("What is Angela's birth name?")
     # doc = nlp("Which rivers and lakes traverse Alaska")
+    doc = nlp("Who is the professor and TA of NLU?")
+
 
     triples = get_user_triples(doc)
     print("===================TRIPLES=====================")
     for subject, predicate, obj in triples:
-        print(subject.text, predicate, obj.text)
+        print(subject, predicate, obj)
     print("===============================================")
